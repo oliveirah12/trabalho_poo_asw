ae694e64199ad2d972e182ba453949568ec09b1d
Matheus <matheus-homem2001@hotmail.com>
2022-11-11 00:29:44 -0300

    a

--- src/view/ClientePrioridade.java
+++ src/view/ClientePrioridade.java
@@ -17,6 +17,7 @@ public class ClientePrioridade extends JFrame {
 
 	/**
 	 * Launch the application.
+	 * 
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
