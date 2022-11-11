5ea25e039f9b483c7ef58de32f02f1d800d14a69
cnesistemas <atendimento.cne@gmail.com>
2022-11-11 13:40:03 -0300

    telas funcionario

---
+++ src/view/FuncionarioAtendente.java
@@ -0,0 +1,196 @@
+package view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import javax.swing.BoxLayout;
+import javax.swing.JDesktopPane;
+import javax.swing.JToolBar;
+import javax.swing.JInternalFrame;
+import java.awt.Font;
+import javax.swing.JCheckBox;
+import javax.swing.SwingConstants;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class FuncionarioAtendente extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FuncionarioAtendente frame = new FuncionarioAtendente();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FuncionarioAtendente() {
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 715, 461);
+		contentPane = new JPanel();
+		contentPane.setBorder(new LineBorder(new Color(0, 0, 0)));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panelCabecalho = new JPanel();
+		panelCabecalho.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		panelCabecalho.setBounds(0, 0, 699, 85);
+		contentPane.add(panelCabecalho);
+		panelCabecalho.setLayout(null);
+		
+		JLabel lblAtendendo = new JLabel("Atendendo:");
+		lblAtendendo.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblAtendendo.setBounds(10, 11, 83, 20);
+		panelCabecalho.add(lblAtendendo);
+		
+		JCheckBox chckbxPreferencial = new JCheckBox("Preferencial");
+		chckbxPreferencial.setEnabled(false);
+		chckbxPreferencial.setBounds(98, 7, 97, 23);
+		panelCabecalho.add(chckbxPreferencial);
+		
+		JCheckBox chckbxNormal = new JCheckBox("Normal");
+		chckbxNormal.setEnabled(false);
+		chckbxNormal.setSelected(true);
+		chckbxNormal.setBounds(98, 33, 97, 23);
+		panelCabecalho.add(chckbxNormal);
+		
+		JCheckBox chckbxServico1 = new JCheckBox("Servi\u00E7o 1");
+		chckbxServico1.setEnabled(false);
+		chckbxServico1.setBounds(220, 7, 97, 23);
+		panelCabecalho.add(chckbxServico1);
+		
+		JCheckBox chckbxServico2 = new JCheckBox("Servi\u00E7o 2");
+		chckbxServico2.setEnabled(false);
+		chckbxServico2.setBounds(220, 33, 97, 23);
+		panelCabecalho.add(chckbxServico2);
+		
+		JCheckBox chckbxServico3 = new JCheckBox("Servi\u00E7o 3");
+		chckbxServico3.setEnabled(false);
+		chckbxServico3.setBounds(220, 59, 97, 23);
+		panelCabecalho.add(chckbxServico3);
+		
+		JButton btnFila = new JButton("Fila");
+		btnFila.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		btnFila.setBounds(613, 19, 76, 45);
+		panelCabecalho.add(btnFila);
+		
+		JPanel panelInfos = new JPanel();
+		panelInfos.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panelInfos.setBounds(0, 399, 699, 23);
+		contentPane.add(panelInfos);
+		panelInfos.setLayout(null);
+		
+		JLabel lblUsuario = new JLabel(" Usu\u00E1rio:");
+		lblUsuario.setBounds(0, 0, 62, 23);
+		panelInfos.add(lblUsuario);
+		
+		JLabel lblUsuarioBd = new JLabel("USUARIO");
+		lblUsuarioBd.setBounds(65, 0, 91, 23);
+		panelInfos.add(lblUsuarioBd);
+		
+		JLabel lblSeparador = new JLabel("|");
+		lblSeparador.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblSeparador.setBounds(163, 0, 16, 23);
+		panelInfos.add(lblSeparador);
+		
+		JLabel lblSeparador2 = new JLabel("|");
+		lblSeparador2.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblSeparador2.setBounds(392, 0, 16, 23);
+		panelInfos.add(lblSeparador2);
+		
+		JLabel lblGuiche = new JLabel("Guich\u00EA:");
+		lblGuiche.setBounds(237, 0, 46, 23);
+		panelInfos.add(lblGuiche);
+		
+		JLabel lblGuicheBd = new JLabel("1");
+		lblGuicheBd.setBounds(316, 0, 46, 23);
+		panelInfos.add(lblGuicheBd);
+		
+		JLabel lblSenha = new JLabel("Senha");
+		lblSenha.setFont(new Font("Tahoma", Font.PLAIN, 22));
+		lblSenha.setBounds(84, 133, 60, 27);
+		contentPane.add(lblSenha);
+		
+		JLabel lblSenhaBd = new JLabel("SENHABD");
+		lblSenhaBd.setHorizontalAlignment(SwingConstants.CENTER);
+		lblSenhaBd.setFont(new Font("Tahoma", Font.PLAIN, 40));
+		lblSenhaBd.setBounds(0, 213, 234, 49);
+		contentPane.add(lblSenhaBd);
+		
+		JLabel lblStatus = new JLabel("Status");
+		lblStatus.setFont(new Font("Tahoma", Font.PLAIN, 22));
+		lblStatus.setBounds(342, 133, 60, 27);
+		contentPane.add(lblStatus);
+		
+		JLabel lblStatusBd = new JLabel("STATUS BD");
+		lblStatusBd.setHorizontalAlignment(SwingConstants.CENTER);
+		lblStatusBd.setFont(new Font("Tahoma", Font.PLAIN, 40));
+		lblStatusBd.setBounds(225, 213, 309, 49);
+		contentPane.add(lblStatusBd);
+		
+		JButton btnFinalizar = new JButton("Finalizar");
+		btnFinalizar.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		btnFinalizar.setBounds(502, 300, 187, 49);
+		contentPane.add(btnFinalizar);
+		
+		JButton btnAusente = new JButton("Ausente");
+		btnAusente.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		btnAusente.setBounds(502, 246, 187, 49);
+		contentPane.add(btnAusente);
+		
+		JButton btnIniciar = new JButton("Iniciar");
+		btnIniciar.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		btnIniciar.setBounds(502, 193, 187, 49);
+		contentPane.add(btnIniciar);
+		
+		JButton btnChamar = new JButton("Chamar");
+		btnChamar.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnChamar.setBounds(502, 140, 92, 49);
+		contentPane.add(btnChamar);
+		
+		JButton btnRepetir = new JButton("Repetir");
+		btnRepetir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnRepetir.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnRepetir.setBounds(597, 140, 92, 49);
+		contentPane.add(btnRepetir);
+		
+		JButton btnVoltar = new JButton("Sair");
+		btnVoltar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				FuncionarioLogin funcionarioLogin = new FuncionarioLogin();
+				funcionarioLogin.setVisible(true);
+				dispose();			
+			}
+		});
+		btnVoltar.setBounds(10, 362, 61, 23);
+		contentPane.add(btnVoltar);
+	}
+}
---
+++ src/view/FuncionarioLogin.java
@@ -0,0 +1,102 @@
+package view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class FuncionarioLogin extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FuncionarioLogin frame = new FuncionarioLogin();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FuncionarioLogin() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 301, 407);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblLogo = new JLabel("S.T.O.P");
+		lblLogo.setFont(new Font("Tahoma", Font.PLAIN, 42));
+		lblLogo.setBounds(73, 11, 140, 51);
+		contentPane.add(lblLogo);
+		
+		JLabel lblLinha = new JLabel("_________________________________");
+		lblLinha.setBounds(43, 48, 198, 14);
+		contentPane.add(lblLinha);
+		
+		JLabel lblLogin = new JLabel("Login");
+		lblLogin.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblLogin.setBounds(43, 135, 44, 17);
+		contentPane.add(lblLogin);
+		
+		textField = new JTextField();
+		textField.setBounds(43, 155, 198, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblMatricula = new JLabel("Matr\u00EDcula");
+		lblMatricula.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblMatricula.setBounds(43, 205, 71, 17);
+		contentPane.add(lblMatricula);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(43, 225, 198, 20);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JButton btnEntrar = new JButton("Entrar");
+		btnEntrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				 
+				//Código para fazer o login e conectar com banco
+				
+				FuncionarioAtendente funcionarioAtendente = new FuncionarioAtendente();
+				funcionarioAtendente.setVisible(true);
+				dispose();
+				
+			}
+		});
+		btnEntrar.setBounds(162, 288, 79, 23);
+		contentPane.add(btnEntrar);
+		
+		JButton btnSair = new JButton("Sair");
+		btnSair.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(1);
+			}
+		});
+		btnSair.setBounds(43, 288, 79, 23);
+		contentPane.add(btnSair);
+	}
+}
