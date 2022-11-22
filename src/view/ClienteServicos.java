65c14f386a8262025d536b88aca375e62a4d2489
Aluno <Aluno@A406-411864>
2022-11-22 20:00:45 -0300

    teste

--- src/view/ClienteServicos.java
+++ src/view/ClienteServicos.java
@@ -20,7 +20,7 @@ public class ClienteServicos extends JFrame {
 	private JPanel contentPane;
 
 	/**
-	 * Launch the application.
+	 * Launch the application.wrerwrerw
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
